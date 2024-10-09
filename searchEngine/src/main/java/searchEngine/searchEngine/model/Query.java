9776605d29f356b32031a48cc6a88c1d8021cb77
Polina Pavlova <polly.nov@gmail.com>
2024-10-09 11:45:00 +0200

    Field Id added to Query so that it matches the data base entity.

--- searchEngine/src/main/java/searchEngine/searchEngine/model/Query.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/model/Query.java
@@ -1,7 +1,20 @@
 package searchEngine.searchEngine.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Query {
-    private String text;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private final String text;
+
+    public Query(String text) {
+        this.text = text;
+    }
 
     public String getText() {
         return text;
