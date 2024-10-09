092ca8741108beba52d1f997e248b6c3cdcc62cc
Polina Pavlova <polly.nov@gmail.com>
2024-10-09 17:28:07 +0200

    QueryController created to list the results of resolved query.

---
+++ searchEngine/src/main/java/searchEngine/searchEngine/controller/QueryController.java
@@ -0,0 +1,14 @@
+package searchEngine.searchEngine.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+
+    @GetMapping("/list")
+    public void getResponse() {}
+
+}
