a2178f5ea484ab88e30bb2cece6292ea9da9a528
Polina Pavlova <polly.nov@gmail.com>
2024-10-10 14:49:04 +0200

    Added temporary method to test connection with SQL database.

--- searchEngine/src/main/java/searchEngine/searchEngine/SearchEngineApplication.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/SearchEngineApplication.java
@@ -1,13 +1,26 @@
 package searchEngine.searchEngine;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import searchEngine.searchEngine.model.Query;
+import searchEngine.searchEngine.repository.SQLRepo;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class SearchEngineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchEngineApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(SQLRepo sqlRepo) {
+		return args -> {
+			sqlRepo.saveAndFlush(new Query("dog"));
+		};
 	}
 
 }
