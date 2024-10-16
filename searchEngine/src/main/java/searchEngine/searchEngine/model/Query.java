336bca22c0b68d0e86233b67778ddb62609389ad
Polina Pavlova <polly.nov@gmail.com>
2024-10-16 10:29:47 +0200

    Added new fields to save more info about a query.

--- searchEngine/src/main/java/searchEngine/searchEngine/model/Query.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/model/Query.java
@@ -1,9 +1,9 @@
 package searchEngine.searchEngine.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 /**
  * Represents search query on a website.
@@ -14,13 +14,56 @@ public class Query {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "text")
     private final String text;
+
+    @Column(name = "sort_field")
+    private final String sortField;
+
+    @Column(name = "sort_direction")
+    private final SortDirection sortDirection;
+
+    @CreationTimestamp
+    @Column(name = "searched_at", updatable = false)
+    private LocalDateTime searchedAt;
+
+
+
+
 
     public Query(String text) {
         this.text = text;
+        this.sortField = "";
+        this.sortDirection = SortDirection.ASC;
+    }
+
+    public Query(String text, String sortField, SortDirection sortDirection) {
+        this.text = text;
+        this.sortField = sortField;
+        this.sortDirection = sortDirection;
+    }
+
+
+
+
+    public Long getId() {
+        return id;
     }
 
     public String getText() {
         return text;
+    }
+
+    public String getSortField() {
+        return sortField;
+    }
+
+    public SortDirection getSortDirection() {
+        return sortDirection;
+    }
+
+    public LocalDateTime getSearchedAt() {
+        return searchedAt;
     }
 }
