efdc9fe0ec419a6ed0103b0e401925f65e123211
Polina Pavlova <polly.nov@gmail.com>
2024-10-16 12:04:34 +0200

    Implemented custom methods to search for queries.

--- searchEngine/src/main/java/searchEngine/searchEngine/service/QuerySQLService.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/service/QuerySQLService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 import searchEngine.searchEngine.model.Query;
 import searchEngine.searchEngine.repository.SQLRepo;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
 public class QuerySQLService {
     private final SQLRepo sqlRepo;
@@ -19,4 +22,33 @@ public class QuerySQLService {
     public void delete(Query query) {
         sqlRepo.delete(query);
     }
-}
+
+    public List<Query> getAllQueries() {
+        return sqlRepo.findAll();
+    }
+
+    public List<Query> getQueriesByText(String text) {
+        return sqlRepo.findByText(text);
+    }
+
+    public List<Query> getQueriesDistinctByText(String text) {
+        return sqlRepo.findDistinctByText(text);
+    }
+
+    public List<Query> getQueriesByDate(LocalDate date) {
+        return sqlRepo.findByDate(date);
+    }
+
+    public List<Query> getQueriesBySortField(String sortField) {
+        return sqlRepo.findBySortField(sortField);
+    }
+
+    public List<Query> getAllQueriesFromToday() {
+        return sqlRepo.findDistinctByDateAfter(LocalDate.now());
+    }
+
+    public List<Query> getAllQueriesFromLastWeek() {
+        return sqlRepo.findDistinctByDateAfter(LocalDate.now()
+                                                .minusWeeks(1));
+    }
+ }
