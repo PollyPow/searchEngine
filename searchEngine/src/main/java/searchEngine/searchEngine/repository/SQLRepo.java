caa0a2e278e844472abfbeb4160f6ebf4e91cdd6
Polina Pavlova <polly.nov@gmail.com>
2024-10-16 10:56:36 +0200

    Added custom methods to the interface to be able to search by query parameters.

--- searchEngine/src/main/java/searchEngine/searchEngine/repository/SQLRepo.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/repository/SQLRepo.java
@@ -3,7 +3,16 @@ package searchEngine.searchEngine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import searchEngine.searchEngine.model.Query;
+import searchEngine.searchEngine.model.SortDirection;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface SQLRepo extends JpaRepository<Query, Long> {
+    List<Query> findByText(String text);
+    List<Query> findDistinctByText(String text);
+    List<Query> findDistinctByTextAndSortField(String text, String sortField);
+    List<Query> findDistinctByTextAndSortFieldAndSortDirection(String text, String sortField, SortDirection sortDirection);
+    List<Query> findDistinctByTextAndDate(String text, LocalDate date);
 }
