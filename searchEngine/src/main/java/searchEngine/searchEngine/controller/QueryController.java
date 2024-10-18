f0b63861550df9fa9eaab18f3176ac49f93aabb7
Polina Pavlova <polly.nov@gmail.com>
2024-10-18 16:38:49 +0200

    Added method to display queries.

--- searchEngine/src/main/java/searchEngine/searchEngine/controller/QueryController.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/controller/QueryController.java
@@ -1,16 +1,23 @@
 package searchEngine.searchEngine.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import searchEngine.searchEngine.model.Query;
+import searchEngine.searchEngine.service.QuerySQLService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/query")
 public class QueryController {
+    @Autowired
+    private QuerySQLService service;
 
-    @GetMapping("/list")
-    public void getResponse() {
-        // TODO implement output building for frontend
+    @GetMapping("/history")
+    public List<Query> getHistory() {
+        return service.getAllQueries();
     }
 
 }
