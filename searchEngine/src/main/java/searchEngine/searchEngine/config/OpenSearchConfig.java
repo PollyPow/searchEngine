7da6007a8c847a9cb0a88f16b9b2c8b1ff6f6125
Polina Pavlova <polly.nov@gmail.com>
2024-10-26 13:50:50 +0200

    Added configuration data to connect with OpenSearch.

--- searchEngine/src/main/java/searchEngine/searchEngine/config/OpenSearchConfig.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/config/OpenSearchConfig.java
@@ -5,14 +5,31 @@ import org.opensearch.client.RestClient;
 import org.opensearch.client.json.jackson.JacksonJsonpMapper;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenSearchConfig {
+
+    @Value("${opensearch.url}")
+    private String url;
+
+    @Value("${opensearch.username}")
+    private String username;
+
+    @Value("${opensearch.password}")
+    private String password;
+
+    @Value("${opensearch.indexes.my_pets}")
+    private String petsIndex;
+
+    @Value("${opensearch.indexes.zoo_animals}")
+    private String animalsIndex;
+
     @Bean
     public OpenSearchClient openSearchClient() {
-        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200, "http")).build();
+        RestClient restClient = RestClient.builder(HttpHost.create(url)).build();
         RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
 
         return new OpenSearchClient(transport);
