c908865396a9b76fc8079a7ab79526eee36cb885
Polina Pavlova <polly.nov@gmail.com>
2024-10-26 13:50:01 +0200

    Created a test for my_pets index service.

---
+++ searchEngine/src/test/java/searchEngine/searchEngine/service/OpenSearchPetsServiceTests.java
@@ -0,0 +1,47 @@
+package searchEngine.searchEngine.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch.core.IndexRequest;
+import org.opensearch.client.opensearch.core.SearchResponse;
+import org.opensearch.client.opensearch.core.search.Hit;
+import org.springframework.beans.factory.annotation.Autowired;
+import searchEngine.searchEngine.model.MyPetsIndex;
+
+import java.util.List;
+
+public class OpenSearchPetsServiceTests {
+
+    @Autowired
+    private OpenSearchClient client;
+
+    @Autowired
+    private OpenSearchPetsService service;
+
+    @Test
+    public void OpenSearchService_GetPetsByName_FindAllPetsByName () {
+        String index = "my_pets";
+        String testName = "TestName";
+        MyPetsIndex pet = new MyPetsIndex();
+        pet.setName(testName);
+        int total = 10;
+        for(int j = 0; j < total; ++j) {
+            IndexRequest<MyPetsIndex> request = IndexRequest.of(i -> i.index(index)
+                                                                        .id(null)
+                                                                        .document(pet));
+            Assertions.assertDoesNotThrow(() -> { client.index(request); });
+        }
+
+
+
+        SearchResponse<MyPetsIndex> pets = service.getPetsByName(testName);
+        List<MyPetsIndex> listOfPets = pets.hits().hits().stream().map(Hit::source).toList();
+
+
+
+        Assertions.assertNotNull(pets);
+        Assertions.assertEquals(total, pets.hits().hits().size(), String.format("The amount of found pets should be %d", total));
+        Assertions.assertTrue(listOfPets.stream().allMatch(p -> p.getName().equals(testName)));
+    }
+}
