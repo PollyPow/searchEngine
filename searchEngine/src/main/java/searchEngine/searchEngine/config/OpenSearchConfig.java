60baabc989fc964bd462a59f749660a51d35c9eb
Polina Pavlova <polly.nov@gmail.com>
2024-11-01 11:32:53 +0100

    Added SSL configuration.

--- searchEngine/src/main/java/searchEngine/searchEngine/config/OpenSearchConfig.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/config/OpenSearchConfig.java
@@ -1,18 +1,33 @@
 package searchEngine.searchEngine.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.opensearch.client.RestClient;
+import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.client.transport.Transport;
 import org.opensearch.client.transport.rest_client.RestClientTransport;
 import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.data.client.osc.OpenSearchConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
 
 @Configuration
 public class OpenSearchConfig {
@@ -34,10 +49,34 @@ public class OpenSearchConfig {
 
     @Bean
     public OpenSearchClient openSearchClient() {
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(this.username, this.password));
 
-        RestClient restClient = RestClient.builder(HttpHost.create(url)).build();
-        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        RestClient client = RestClient.builder(new HttpHost("localhost", 9200, "https"))
+                .setHttpClientConfigCallback(httpClientBuilder -> {
+                    try {
+                        SSLContext sslContext = SSLContextBuilder.create()
+                                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                                .build();
+
+                        return httpClientBuilder
+                                .setSSLContext(sslContext)
+                                //.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                                .setDefaultCredentialsProvider(credentialsProvider);
+                    } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+                        throw new RuntimeException("Failed to initialize SSL context for OpenSearch client", e);
+                    }
+                })
+                .build();
+
+        OpenSearchTransport transport = new RestClientTransport(
+                client,
+                new JacksonJsonpMapper()
+        );
+
 
         return new OpenSearchClient(transport);
     }
+
 }
