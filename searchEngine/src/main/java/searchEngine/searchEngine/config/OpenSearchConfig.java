73fa4d43188de2e09f0f99fbd99bf95028a0d943
Polina Pavlova <polly.nov@gmail.com>
2024-11-06 09:46:45 +0100

    Refactored Opensearch config so that it doesnot create a rest client but uses client configuration.

--- searchEngine/src/main/java/searchEngine/searchEngine/config/OpenSearchConfig.java
+++ searchEngine/src/main/java/searchEngine/searchEngine/config/OpenSearchConfig.java
@@ -1,58 +1,19 @@
 package searchEngine.searchEngine.config;
 
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.TrustAllStrategy;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.opensearch.client.RestClient;
-import org.opensearch.client.RestClientBuilder;
-import org.opensearch.client.opensearch.OpenSearchClient;
-import org.opensearch.client.transport.OpenSearchTransport;
-import org.opensearch.client.transport.Transport;
-import org.opensearch.client.transport.rest_client.RestClientTransport;
-import org.opensearch.client.json.jackson.JacksonJsonpMapper;
 import org.opensearch.data.client.osc.OpenSearchConfiguration;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.time.Duration;
 
 @Configuration
-public class OpenSearchConfig {
+public class OpenSearchConfig extends OpenSearchConfiguration{
 
-    @Value("${opensearch.url}")
-    private String url;
+    @Value("${opensearch.hostAndPort}")
+    private String hostAndPort;
 
-    @Value("${opensearch.username}")
-    private String username;
-
-    @Value("${opensearch.password}")
-    private String password;
-
-    @Value("${opensearch.indexes.my_pets}")
-    private String petsIndex;
-
-    @Value("${opensearch.indexes.zoo_animals}")
-    private String animalsIndex;
-
-    @Bean
-    public OpenSearchClient openSearchClient() {
-        RestClient client = RestClient.builder(new HttpHost("localhost", 9200, "http")).build();
-        OpenSearchTransport transport = new RestClientTransport(client, new JacksonJsonpMapper());
-
-        return new OpenSearchClient(transport);
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder().connectedTo(hostAndPort).build();
     }
-
 }
