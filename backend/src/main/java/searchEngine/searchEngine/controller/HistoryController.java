5076de4e740aeba93e4adb2546b35288c5b962c2
Polina Pavlova <polly.nov@gmail.com>
2024-11-24 10:39:56 +0100

    Renamed history controller for better understanding.

--- backend/src/main/java/searchEngine/searchEngine/controller/HistoryController.java
+++ backend/src/main/java/searchEngine/searchEngine/controller/HistoryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/query")
-public class QueryController {
+public class HistoryController {
     @Autowired
     private HistoryServiceImpl service;
 
