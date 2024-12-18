09bfa5d54acd1427f21cb91408d4c20e8fd3b3cd
Polina Pavlova <polly.nov@gmail.com>
2024-12-18 13:11:29 +0100

    Added a description of an accessible format of range query on website.

--- frontend/spring-boot/src/main/resources/static/server/angular-app-manifest.mjs
+++ frontend/spring-boot/src/main/resources/static/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 492, hash: 'e22435b0bdb38d79a9fbac856cd2ed03ec0c04bf25747aab7d5030b76db46b91', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1005, hash: 'e586e33960b6ddb3f46b2ef43ee56fbb993c09ddabc3bf0fc6f5e5f0d9e0a1d1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 492, hash: 'a2f8f73f1befb282bc7c1839c502a01da3cfcd9fd8e17cf4e6c74cd15ffba1db', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1005, hash: '4dac82cb1facf935b9d2068d146cfdfa30416cf97522716bac7e734d08534293', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
